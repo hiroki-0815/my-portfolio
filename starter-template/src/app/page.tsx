6a710c3938e143552ac480368a2160d67ab8da99
hiroki <haohuirixia851@gmail.com>
2024-09-30 19:56:44 +0900

    add projects

--- starter-template/src/app/page.tsx
+++ starter-template/src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+import ProjectsSection from "@/sections/Projects";
 
 export default function Home() {
   return (
-    <div>
+    <div className="px-3">
       <Header />
+      <HeroSection />
+      <ProjectsSection />
     </div>
   );
 }
--- starter-template/src/sections/Projects.tsx
+++ starter-template/src/sections/Projects.tsx
@@ -1,6 +1,9 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpIcon from "@/assets/icons/arrow-up-right.svg";
 
 const portfolioProjects = [
   {
@@ -41,6 +44,56 @@ const portfolioProjects = [
   },
 ];
 
-export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+const ProjectsSection = () => {
+  return (
+    <section className="flex flex-col items-center pb-16">
+      <div className="container"></div>
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
+          Real-world Results
+        </p>
+      </div>
+      <h2 className="font-serif text-3xl text-center mt-6 font-semibold">
+        Featured Projects
+      </h2>
+      <p className="text-white/60 mt-4">
+        See how I transformed concepts into engaging digital experiences.
+      </p>
+      <div>
+        {portfolioProjects.map((project) => (
+          <div
+            className=" p-6 flex flex-col mt-10 bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
+            key={project.title}
+          >
+            <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text font-semibold tracking-wide text-sm uppercase inline-flex gap-1 ">
+              <span>{project.company}</span>
+              <span>&bull; </span>
+              <span> {project.year}</span>
+            </div>
+            <h3 className="font-serif text-2xl mt-2 font-semibold tracking-widest">
+              {project.title}
+            </h3>
+            <hr className="border-t-2 my-4 border-white/5" />
+            <ul className="flex flex-col gap-4 mb-1">
+              {project.results.map((result) => (
+                <li className="flex flex-row gap-2 items-center text-white/50">
+                  <CheckIcon className="size-5" />
+                  <span className="text-sm ">{result.title} </span>
+                </li>
+              ))}
+            </ul>
+            <a href={project.link}>
+              <button className="bg-white text-gray-950 h-12 w-full rounded-md font-semibold inline-flex items-center justify-center my-6">
+                <span>View Live Site</span>
+                <ArrowUpIcon className="size-4" />
+              </button>
+            </a>
+            <Image src={project.image} alt={project.title} className="mb-4" />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
+
+export default ProjectsSection;
