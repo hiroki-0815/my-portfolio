7dc0112192b0e0d5f61ca894d1cc49939b5cdf72
hiroki <haohuirixia851@gmail.com>
2024-10-01 12:47:10 +0900

    build section header component

--- starter-template/src/app/page.tsx
+++ starter-template/src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import ProjectsSection from "@/sections/Projects";
 import Tape from "@/sections/Tape";
+import About from "@/sections/About";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <ProjectsSection />
       <Tape />
+      <About />
     </div>
   );
 }
--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -1,3 +1,16 @@
-export const AboutSection = () => {
-  return <div>About Section</div>;
+import SectionHeader from "@/components/SectionHeader";
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <SectionHeader
+        eyebrow="about me"
+        title="a glimpse into my word"
+        description="Learn more about who I am. what I do, and what inspires me"
+      />
+    </div>
+  );
 };
+
+export default About;
