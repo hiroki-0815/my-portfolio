327147ffd52955f98ac6bbf4dcf62270d2955988
hiroki <haohuirixia851@gmail.com>
2024-10-03 17:25:05 +0900

    add orbit & spin animation

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -20,35 +20,106 @@ export const HeroSection = () => {
         <div className="size-[820px] hero-ring"></div>
         <div className="size-[1020px] hero-ring"></div>
         <div className="size-[1220px] hero-ring"></div>
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-600" />
-        </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-600" />
-        </HeroOrbit>
-        <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-emerald-600" />
-        </HeroOrbit>
-        <HeroOrbit size={430} rotation={-15}>
+
+        <HeroOrbit
+          size={430}
+          rotation={-15}
+          shouldOrbit
+          orbitDuration="30s"
+          shouldSpin
+          spinDuration="10s"
+        >
           <SparkleIcon className="size-8 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={440} rotation={79}>
+        <HeroOrbit
+          size={440}
+          rotation={79}
+          shouldOrbit
+          orbitDuration="32s"
+          shouldSpin
+          spinDuration="10s"
+        >
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={530} rotation={178}>
+        <HeroOrbit
+          size={530}
+          rotation={178}
+          shouldOrbit
+          orbitDuration="34s"
+          shouldSpin
+          spinDuration="10s"
+        >
           <SparkleIcon className="size-10 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={710} rotation={144}>
+        <HeroOrbit
+          size={520}
+          rotation={-42}
+          shouldOrbit
+          orbitDuration="36s"
+          shouldSpin
+          spinDuration="10s"
+        >
+          <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={550}
+          rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="10s"
+        >
+          <StarIcon className="size-12 text-emerald-600" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={590}
+          rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="10s"
+        >
+          <StarIcon className="size-8 text-emerald-600" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={650}
+          rotation={-5}
+          shouldOrbit
+          orbitDuration="42s"
+          shouldSpin
+          spinDuration="10s"
+        >
+          <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={710}
+          rotation={144}
+          shouldOrbit
+          orbitDuration="44s"
+          shouldSpin
+          spinDuration="10s"
+        >
           <SparkleIcon className="size-14 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={720} rotation={85}>
+        <HeroOrbit
+          size={720}
+          rotation={85}
+          shouldOrbit
+          orbitDuration="46s"
+          shouldSpin
+          spinDuration="10s"
+        >
           <SparkleIcon className="size-3 rounded-full text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={520} rotation={-42}>
-          <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5}>
-          <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
+        <HeroOrbit
+          size={800}
+          rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="10s"
+        >
+          <StarIcon className="size-28 text-emerald-600" />
         </HeroOrbit>
       </div>
       <div className="container">
