02f663ec0a763a5d5d22aaa01c8783e71f35e9ad
hiroki <haohuirixia851@gmail.com>
2024-10-04 09:48:47 +0900

    make the Tape sec simpler

--- starter-template/src/sections/Tape.tsx
+++ starter-template/src/sections/Tape.tsx
@@ -1,5 +1,4 @@
 import StarIcon from "@/assets/icons/star.svg";
-import { Fragment } from "react";
 
 const Tape = () => {
   const words = [
@@ -18,19 +17,15 @@ const Tape = () => {
   return (
     <div className="py-16">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-hidden -rotate-3 py-3">
-        <div className=" items-center [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] ">
-          <div className=" flex flex-none gap-4 items-center animate-move-left [animation-duration:30s]">
-            {[...new Array(2)].fill(0).map((_, index) => (
-              <Fragment key={index}>
-                {words.map((word) => (
-                  <div className="inline-flex gap-4" key={word}>
-                    <span className="text-black font-bold text-sm whitespace-nowrap uppercase">
-                      {word}
-                    </span>
-                    <StarIcon className="size-6 text-black -rotate-10" />
-                  </div>
-                ))}
-              </Fragment>
+        <div className="items-center [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 items-center animate-move-left [animation-duration:30s]">
+            {words.concat(words).map((word) => (
+              <div className="inline-flex gap-4" key={word}>
+                <span className="text-black font-bold text-sm whitespace-nowrap uppercase">
+                  {word}
+                </span>
+                <StarIcon className="size-6 text-black -rotate-10" />
+              </div>
             ))}
           </div>
         </div>
